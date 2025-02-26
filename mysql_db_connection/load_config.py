a28f777103498765a4be34f9fae88c563a1d1f45
Felipe Garaycochea Lozada <fgaraycochealozada@rccl.com>
2025-02-26 11:36:47 -0500

    yaml config file_path changed

--- mysql_db_connection/load_config.py
+++ mysql_db_connection/load_config.py
@@ -1,6 +1,6 @@
 import yaml
 
 
-def load_config(file_path="config.yaml"):
+def load_config(file_path="config/database_config.yaml"):
     with open(file_path, "r") as file:
         return yaml.safe_load(file)
